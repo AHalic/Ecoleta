45ef45b29a285b887e75aa2f238990688987fa23
AHalic <sophie.gama@aluno.ufes.br>
2022-10-09 23:48:25 -0300

    :construction: adds cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
+import path from 'path';
 
 // run the program as:
 // npm run dev
 
 const app = express();
+
 app.use(express.json());
+app.use(routes);
 
-
+// para acessar as imagens ou pdfs de forma estatica
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
