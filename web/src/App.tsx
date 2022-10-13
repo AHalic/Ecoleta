724922a55613237c510a7ff45dadf3c580941495
AHalic <sophie.gama@aluno.ufes.br>
2022-10-12 21:12:01 -0300

    :books: :coffin: adds studies to header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -13,33 +13,5 @@ function App() {
 }
 
 
-/*
-import React, { useState } from 'react';
-
-import Header from './Header';
-
-// JSX: Sintaxe de XML dentro do JavaScript
-
-function App() {
-	// conceito de imutabilidade do react: 
-	// não é possivel fazer um simples counter++
-	// é necessario criar um novo valor para o estado
-	const [counter, setCounter] = useState(0); // retorna [valor do estado, função para atualizar o valor do estado]
-
-	function handleButtonClick() {
-		setCounter(counter + 1);
-	}
-
-	return (
-		<div>
-			<Header title={`Contador: ${counter}`}/> {// Componente Header Ecoleta }
-
-			<h1>{counter}</h1>
-			<button type='button' onClick={handleButtonClick}>Aumentar</button>
-		</div>
-	);
-}
-*/
-
 
 export default App;
--- web/src/Header.tsx
+++ web/src/Header.tsx
@@ -14,5 +14,32 @@ const Header:React.FC<HeaderProps> = (props) => {
   );
 }
 
+/*
+import React, { useState } from 'react';
+
+import Header from './Header';
+
+// JSX: Sintaxe de XML dentro do JavaScript
+
+function App() {
+	// conceito de imutabilidade do react: 
+	// não é possivel fazer um simples counter++
+	// é necessario criar um novo valor para o estado
+	const [counter, setCounter] = useState(0); // retorna [valor do estado, função para atualizar o valor do estado]
+
+	function handleButtonClick() {
+		setCounter(counter + 1);
+	}
+
+	return (
+		<div>
+			<Header title={`Contador: ${counter}`}/> {// Componente Header Ecoleta }
+
+			<h1>{counter}</h1>
+			<button type='button' onClick={handleButtonClick}>Aumentar</button>
+		</div>
+	);
+}
+*/
 
 export default Header;
